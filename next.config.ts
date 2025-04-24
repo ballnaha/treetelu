27927e2b285bf2d2f550bcc0c32569593f4bcd93
thanya <ball_t1@hotmail.com>
2025-04-24 16:58:50 +0700

    change next.config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-/* eslint-disable */
+/* eslint-disable */ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {},
   images: {
+    //
     domains: ['localhost'],
     unoptimized: process.env.NODE_ENV === 'development',
   },
