26a0d6299bf886461da481e658135b8edee85682
ballnaha <ball_t1@hotmail.com>
2025-04-25 08:29:48 +0700

    edit next.config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['http://168.231.118.94:3001'],
-  allowedOrigins: ['http://168.231.118.94:3001'],
   reactStrictMode: true,
   experimental: {},
   images: {
