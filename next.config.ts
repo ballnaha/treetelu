d0e645927957951dcbd184d5f116eee2ccae3a1a
thanya <ball_t1@hotmail.com>
2025-04-25 20:01:20 +0700

    fix node

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 /* eslint-disable */ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['http://168.231.118.94:3001'],
+  allowedDevOrigins: [
+    'http://168.231.118.94:3001',
+    'http://168.231.118.94',
+    'https://168.231.118.94:3001',
+    'https://168.231.118.94'
+  ],
   reactStrictMode: true,
   experimental: {},
   images: {
     //
-    domains: ['localhost'],
+    domains: ['localhost', '168.231.118.94'],
     unoptimized: process.env.NODE_ENV === 'development',
   },
 };
